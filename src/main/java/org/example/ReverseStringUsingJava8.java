203685debe6b36e84a8e13199f6124ec591ef8cf
Bhar32arya <bhar32arya@gmail.com>
2024-02-23 19:49:17 +0530

    updated with another approach

--- src/main/java/org/example/ReverseStringUsingJava8.java
+++ src/main/java/org/example/ReverseStringUsingJava8.java
@@ -7,5 +7,17 @@ public class ReverseStringUsingJava8 {
         String str = "example of reverse string";
         IntStream intStream = IntStream.range(0, str.length());
         intStream.forEach(i -> System.out.print(str.charAt(str.length() - 1 - i)));
+
+        System.out.println();
+
+        IntStream.range(0, str.length())
+                        .map(i -> str.length() - 1 - i)
+                                .mapToObj(c -> str.charAt(c))
+                                        .forEach(System.out::print);
+
+        System.out.println();
+
+        StringBuilder sb = new StringBuilder(str).reverse();
+        System.out.println(sb);
     }
 }
